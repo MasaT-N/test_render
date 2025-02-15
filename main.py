7a080899617cdb959524ef43e0a812760161e687
Masakazu Takai <masakazu.t0531@gmail.com>
2025-02-15 21:35:24 +0900

    ver1.3

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask,request
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/post_data', methods=['GET', 'POST'])
+def check():
+    if request.method == 'POST':
+        data = request.json['key']
+    else:
+        data = 'no data'
+    return data
 
 if __name__ == "__main__":
     # webサーバー立ち上げ
