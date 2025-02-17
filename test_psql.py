e03ccb9aafcefef49db17d0a44fbfddc989044dc
Masakazu Takai <masakazu.t0531@gmail.com>
2025-02-17 21:37:15 +0900

    データ確認用のSQLを改修

--- test_psql.py
+++ test_psql.py
@@ -4,6 +4,7 @@ import psycopg2
 from psycopg2.extras import DictCursor
 from psycopg2.extensions import connection
 import os
+import json
 
 database_url = os.environ.get('DATABASE_URL')
 
@@ -14,19 +15,40 @@ def get_connection():
 with get_connection() as connection:
     # カーソルをオープンします
     with connection.cursor(cursor_factory=DictCursor) as cursor:
-        # テーブルを削除します。
+        # # テーブルを削除します。
         # sql = "Drop table if exists json_table;"
         # cursor.execute(sql)
 
-        # テーブル作成SQLを定義します
-        sql = "CREATE TABLE if not exists json_table (id SERIAL PRIMARY KEY, data JSONB, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
-        # SQLを実行します
-        cursor.execute(sql)
+        # # テーブル作成SQLを定義します
+        # sql = "CREATE TABLE if not exists json_table (id SERIAL PRIMARY KEY, data JSONB, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
+        # # SQLを実行します
+        # cursor.execute(sql)
 
         # データ取得します。
-        sql = "Select * from json_table;"
+        # sql = "Select count(*) from purchase_requisition;"
+
+        sql = "SET timezone TO 'Asia/Tokyo';"
+        cursor.execute(sql)
+
+        sql = """
+                SELECT
+                    document_id, 
+                    document_number, 
+                    document_title, 
+                    request_user, 
+                    request_group, 
+                    request_factory, 
+                    amount, 
+                    flow_status, 
+                    (end_date AT TIME ZONE 'UTC' AT TIME ZONE 'Asia/Tokyo') as end_date,
+                    (created_at AT TIME ZONE 'UTC' AT TIME ZONE 'Asia/Tokyo') as created_at
+                FROM purchase_requisition;
+            """
         cursor.execute(sql)
         res = cursor.fetchall()
-        print(res)
+        for row in res:
+            
+            print(row)
+   
 
 
